77b0c4cddaa9249d48a70494a7baea97116ea6ee
MD. SHARIF UDDIN <101212925+sharif4u@users.noreply.github.com>
2024-02-01 23:29:18 +0600

    Create datatype.py

---
+++ datatype.py
@@ -0,0 +1,10 @@
+#dataType
+# 101 = int
+# Sharif ahemd = string
+# 10.5 = float
+
+# Take input from key board with typecasting
+firstNum = 11
+secondNum = input("Enter a Number: ")
+sum = firstNum + int(secondNum)
+print("Sum of two number: ",sum)
